abf7b17b3148f4afec1e7e6088ea52922964b625
MohammadSheakh <mohammad.sheakh@gmail.com>
2022-12-27 22:31:35 +0600

    function and loop

--- function.js
+++ function.js
@@ -1,4 +1,87 @@
-// lecture ---------------------------------------- 9
+// Function Declaration 
+/**
+ * named function
+ * variable function 
+ * arrow function 
+ * inline arrow function  
+ *  Minimal function  
+ * Return data
+ * return function 
+ * 
+ * execution 
+ * 
+ * function call 
+ * self executing function
+ * 
+ * scope 
+ * nested function 
+ * how to give input to a function 
+ * Arguments 
+ */
+function function1(){ // Declaration .. 
+    console.log(Date.now()); // Date hocche ekta built in class 
+}
+// calling 
+function1();
+// create a function and store it into a variable 
+const now  = function () {
+    console.log(Date.now());
+}
+now(); // now function ta call korleo .. same kaj korbe .. 
+
+// named function er scoping style different 
+
+// arrow function 
+const arrowFunction = () => {
+    // function er body 
+    // variable na dile .. call korbo kivabe .. 
+    console.log("test arrow function ");
+}
+arrowFunction(); // arrow function calling .. 
+
+// inline arrow function 
+const calculation = (num, fn) => {
+    let result  = 0;
+    for(let i = 0; i < num ; i++){
+        let temp = fn(i);
+        result +=temp;
+    }
+    return result;
+}
+console.log("inline function")
+const sum = calculation(10, (x) => x+x );
+console.log("SUM calculation function  = ", sum);
+
+const multiplication = calculation(10, (x) => x*x ); // inline function
+console.log("MULTIPLICATION calculation function  = ", multiplication);
+
+// minimul function 
+console.log("minimal function ");
+() => 10; // ei function ta shob shomoy 10 return korbe .. 
+(x) => x*x; // x pathaile .. x*x return korbe .. 
+
+const square = (x) => x*x;
+console.log("square function =>minimal funciton => ", square(10));
+
+// function
+const square2 = (x) => {
+    return fn  = function () {
+        return x*x;
+    }
+    return fn;
+}
+
+
+console.log("square2(10) ... this returns function body ", square2(10)); // it returns function body .. not result ..
+
+console.log("square2(10)() ... this returns result ", square2(10)()); // eita result return kore .. 
+
+
+// self executing function
+(() => {return new Date();})() // she nijei ekta return statement hoye gese .. 
+(new Date());  // 
+
+//--------------------- lecture ---------------------------------------- 9
 console.log("function started ");
 
 var m = { x: 10 };
---
+++ loop.js
@@ -0,0 +1,53 @@
+// -------------------------------- 7 
+let x = 10;
+while(x < 0 ){
+    console.log(x);
+    x++;
+}
+// Now For In Concept 
+j = {
+    name : "mern",
+    type : 'course',
+    lang : "js",
+}
+
+console.log("-------------------------------- Print All Keys of J Array");
+for(let k in j){ // k means keys .. 
+    console.log(k); // print all keys .. 
+}
+/*
+for (let v of j) // v means values 
+{
+    console.log(v); // print all values 
+    // keys gula she iterate korte pare .. kintu value gula she iterate korte
+    // pare na ... 
+}
+*/
+// print values of Arr 
+console.log("-------------------------------------Print Values of Arr");
+arr = [10, 20, 30];
+for (let value of arr){
+    console.log(value);
+}
+
+// ekhon jodi duita jinish ke merge kori .. 
+// mane key value duita kei print korte chai .. 
+
+console.log("-------------------------------- Print All Values of J Array");
+for(let k in j){ // k means keys .. 
+    console.log(j[k]); // print all values  .. 
+}
+
+j["Phone"] = "123";
+
+console.log("-------------------------------- Print All Values of J Array after assign phone = 123");
+for(let k in j){ // k means keys .. 
+    console.log(j[k]); // print all values  .. after assign phone = 123 
+}
+
+/**
+ * basic ... 
+ * 
+ * Iterable thing  -> For of 
+ * Object          -> For in 
+ */
